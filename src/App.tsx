7a6eb4078e8fa21226ee2464b7ece3e12ba6a1a1
gustavofabro <gustavofabro.f@gmail.com>
2021-10-23 08:35:04 -0300

    Add infinite scroll with Intersection Observer

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,67 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import './App.css';
 
-function App() {
-  const perPage = 20;
+interface DataType {
+  id: number
+}
 
-  const [data, setData] = useState(Array.from(Array(perPage).keys()).map(item => (
-    {
-      id: item,
-      description: `Item ${item}`
+function App() {
+  const [current, setCurrent] = useState(0);
+  const [data, setData] = useState<DataType[]>([]);
+  const loadingElementRef = useRef<HTMLDivElement>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setData(Array.from(Array(current).keys()).map(item => ({ id: item })))
+  }, [current]);
+
+  function mockApiCall() {
+    return new Promise<void>((resolve) => {
+      setTimeout(() => resolve(), 5000);
+    });
+  }
+
+  const loadItems = useCallback(() => {
+    setIsLoading(true);
+
+    mockApiCall()
+      .then(() => {
+        setCurrent(current + 10);
+      })
+      .finally(() => setIsLoading(false))
+  }, [current]);
+
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries: IntersectionObserverEntry[]) => {
+        if (!entries[0].isIntersecting || isLoading) {
+          return;
+        }
+
+        loadItems()
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0
+      });
+
+    if (loadingElementRef && loadingElementRef.current) {
+      observer.observe(loadingElementRef.current);
     }
-  )));
+
+    return () => observer.disconnect();
+  }, [loadingElementRef, isLoading, loadItems]);
 
   return (
     <main className="app">
       <div className="list">
-       {data.map(item => (
-         <span key={item.id} className="item">{item.description}</span>
-       ))}
+        {data.map(item => (
+          <div key={item.id} className="item">{`Item ${item.id + 1}`}</div>
+        ))}
+        {isLoading && <i className="loading">Carregando...</i>}
+        <div ref={loadingElementRef}></div>
       </div>
     </main>
   );
